718fdcb4993e43341437fcc91d1d1dc7365548bf
contactlogonova-stack <contact.logonova@gmail.com>
2026-07-27 03:43:19 +0000

    Nav responsive mobile

--- app.js
+++ app.js
@@ -69,3 +69,19 @@ if(contactForm){
         }
     });
 }
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+if(hamburger){
+    hamburger.addEventListener('click', function(){
+        hamburger.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+
+    document.querySelectorAll('.nav-link a').forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navLinks.classList.remove('active');
+        });
+    });
+}
